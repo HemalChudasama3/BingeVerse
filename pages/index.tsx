b14cfd2e912b1bc56f7ab40a3f107c5c5c3c8da8
Hemal Chudasama <hemanl.cce21@sot.pdpu.ac.in>
2024-06-15 13:17:05 +0530

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import { AppProps } from 'next/app';
 import Layout from '../styles/layout';
 
 import { NextPageContext } from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
+import Navbar from '@/components/Navbar';
 
-import useCurrentUser from '@/hooks/useCurrentUser';
 import { redirect } from 'next/dist/server/api-utils';
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -27,15 +27,11 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
 
   return (
     <>
-    <div className='bg-black h-screen w-screen'>
-
-    <h1 className='text-white text-lg'>BingeVerse</h1>
-    <p className='text-white'>Logged in as : {user?.name}</p>
-    <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout</button>
+    <div className='bg-gray-950 h-screen w-screen text-white'>
+      <Navbar />
     </div>
     </>
   );
