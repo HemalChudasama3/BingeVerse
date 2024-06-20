52ce55e8cd1bb490b58b2385662186f35684fe32
Hemal Chudasama <hemanl.cce21@sot.pdpu.ac.in>
2024-06-20 15:25:54 +0530

    Title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>BingeVerse</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
